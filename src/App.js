f3f1e44a2efb0df7f51f0acfa14db77e9ab5f66b
hitiksaini <hitik9045saini@gmail.com>
2020-09-29 13:54:04 +0530

    added logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 5
+        }}
+    />
+);
 function App() {
   return (
     <div className="App">
+    <img src={logo} alt="Logo" />
       <form>
-
                <h3 class="text-center">SIGN UP</h3>
                <h1 class="text-center"> Create your account</h1>
                <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit </p>
@@ -15,13 +23,12 @@ function App() {
 
 
 
-<div class="google-btn">
-  <div class="google-icon-wrapper">
-    <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
-  </div>
-  <p class="btn-text"><b>Sign up with google</b></p>
-</div>
-
+              <div class="google-btn">
+                <div class="google-icon-wrapper">
+                  <img class="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+                </div>
+                <p class="btn-text"><b>Sign up with google</b></p>
+              </div>
 
                </div>
 
