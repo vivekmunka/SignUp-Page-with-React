37b66b6692cf143332eb94c2dfe4eb5c2a4e5bef
hitiksaini <hitik9045saini@gmail.com>
2020-09-29 14:27:26 +0530

    Updates

---
+++ precache-manifest.5a775c1c975b4234af05c305ccaa4869.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985a32e30ee6ea32d95e4800fb6b2688",
+    "url": "/SignUp-Page-with-React/index.html"
+  },
+  {
+    "revision": "6fe583203cb8ea9d2f83",
+    "url": "/SignUp-Page-with-React/static/css/main.9ae3e7f8.chunk.css"
+  },
+  {
+    "revision": "a82d0e9dddbb3e1bba4b",
+    "url": "/SignUp-Page-with-React/static/js/2.7df0e9e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SignUp-Page-with-React/static/js/2.7df0e9e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fe583203cb8ea9d2f83",
+    "url": "/SignUp-Page-with-React/static/js/main.37fc3f40.chunk.js"
+  },
+  {
+    "revision": "25b6924d3d576dbfa2b0",
+    "url": "/SignUp-Page-with-React/static/js/runtime-main.c1016fba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SignUp-Page-with-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
